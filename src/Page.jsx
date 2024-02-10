2f61cc260d64ff77673a910f8263fbe95da9c387
Rana Umar Shakoor <120050041+RanaUmarShakoor@users.noreply.github.com>
2024-02-11 03:35:58 +0500

    Modals and Language dropdown

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,5 +1,10 @@
 import { Container } from "./components/Container";
 import { Navbar } from "./components/Navbar";
+
+import * as Icon from "react-icons/fi";
+import Checkbox from "react-custom-checkbox";
+
+import map_bg from './assets/map_bg.png'
 import star_user from "./assets/star_user.png";
 import cleaning_icon from "./assets/cleaning_icon.png";
 import info_icon from "./assets/info_icon.png";
@@ -8,10 +13,15 @@ import user_orange from "./assets/user_orange_icon.png";
 
 export function Page() {
   return (
-    <main className="w-screen h-screen flex flex-col bg-pink-400">
+    <main className="w-screen h-screen flex flex-col">
       <Navbar />
-      <Container className="flex-1 flex items-center justify-center bg-purple-400">
-        {/* <div className="flex flex-col lg:flex-row gap-8 lg:gap-0 p-8 rounded-[8px] lg:bg-[#000000B2]">
+      <Container style={{backgroundImage: `${map_bg}`}}  className="flex-1 flex items-center justify-center relative z-1">
+        <img src={map_bg} alt="" className="absolute overflow-hidden -z-20 top-0 left-0 w-full h-full object-cover" />
+        <div className="flex absolute -z-10 h-full w-full">
+          <div className="flex-1 bg-gradient-to-r from-[#FFA50000] to-[#FFA5004D]"  />
+          <div className="flex-1 bg-gradient-to-r from-[#0070FF4D] to-[#0070FF1A]"  />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-0 p-8 rounded-[8px] lg:bg-[#000000B2]">
           <Block
             photo={star_user}
             heading="Do you offer cleaning services?"
@@ -24,8 +34,9 @@ export function Page() {
             subheading="Register as a user"
             type="2"
           />
-        </div> */}
-        <RegisterCleanerModal />
+        </div>
+        {/* <RegisterUserModal /> */}
+
         {/* <ForgottenPasswordModal_3 /> */}
         {/* <RegistrationSuccesModal /> */}
       </Container>
@@ -45,7 +56,7 @@ function Block({ photo, heading, subheading, type = "1" }) {
       <h3 className="font-semibold text-2xl md:text-3xl lg:text-4xl">
         {heading}
       </h3>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 mt-auto">
         <a href="">
           <h4 className="font-semibold text-base md:text-xl lg:text-2xl">
             {subheading}
@@ -83,7 +94,7 @@ function RegisterCleanerModal() {
           <label htmlFor="" className="font-medium">
             Upload your photo ID
           </label>
-            <img src={info_icon} alt="" className="h-5 w-5 object-contain" />
+          <img src={info_icon} alt="" className="h-5 w-5 object-contain" />
         </div>
         <button className={" py-2 rounded-lg border-[1px] border-orange-500"}>
           <div className="flex items-center justify-center gap-2 text-orange-500">
@@ -93,7 +104,50 @@ function RegisterCleanerModal() {
         </button>
       </div>
 
-        
+      <div className="flex flex-col gap-2">
+        <Checkbox
+          checked={false}
+          icon={
+            <div
+              style={{
+                display: "flex",
+                flex: 1,
+                backgroundColor: "#FF6B00",
+                alignSelf: "stretch",
+              }}
+            >
+              <Icon.FiCheck color="white" size={20} />
+            </div>
+          }
+          borderColor="#174A41"
+          borderWidth={1}
+          borderRadius={20}
+          style={{ overflow: "hidden" }}
+          size={20}
+          label="I agree with the general terms and conditions"
+        />
+        <Checkbox
+          checked={false}
+          icon={
+            <div
+              style={{
+                display: "flex",
+                flex: 1,
+                backgroundColor: "#FF6B00",
+                alignSelf: "stretch",
+              }}
+            >
+              <Icon.FiCheck color="white" size={20} />
+            </div>
+          }
+          borderColor="#174A41"
+          borderWidth={1}
+          borderRadius={20}
+          style={{ overflow: "hidden" }}
+          size={20}
+          label="I agree to the sending of marketing news"
+        />
+      </div>
 
       <button className={" py-2 rounded-lg bg-orange-500"}>
         <div className="flex items-center justify-center gap-2 text-white">
@@ -101,13 +155,98 @@ function RegisterCleanerModal() {
           <p>Register</p>
         </div>
       </button>
-      <div className="flex justify-center">
+      <div className="flex md:hidden justify-center">
         <button className="font-medium px-1 py-[0.5px] border-[1px] rounded-md border-orange-500 text-orange-500 bg-[#F0F2F5] text-center">
           Close
         </button>
       </div>
     </div>
   );
+}
+
+function RegisterUserModal(){
+  return (
+    <div className="flex flex-col gap-4 p-6 rounded-[8px] bg-white">
+      <div className="hidden lg:flex justify-center">
+        <div className={"p-3 rounded-full bg-blue-500"}>
+          <img src={cleaning_icon} alt="" className="h-10 w-10 object-contain" />
+        </div>
+      </div>
+      <FormInput
+        label="Name"
+        type="text"
+        placeholder="Jozko"
+        color={2}
+      />
+      <FormInput
+        label="Surname"
+        type="text"
+        placeholder="Mrkvička"
+        color={2}
+      />
+      <FormInput label="Email" type="email" placeholder="Mrkvička@gmail.com" color={2} />
+      <FormInput label="Password" type="password" placeholder="**********" color={2} />
+      <FormInput label="Repeat the password" type="password" placeholder="**********" color={2} />
+      <FormInput label="Mobile number" type="text" placeholder="0900 000 000" color={2} />
+      
+      <div className="flex flex-col gap-2">
+        <Checkbox
+          checked={false}
+          icon={
+            <div
+              style={{
+                display: "flex",
+                flex: 1,
+                backgroundColor: "#0094FF",
+                alignSelf: "stretch",
+              }}
+            >
+              <Icon.FiCheck color="white" size={20} />
+            </div>
+          }
+          borderColor="#174A41"
+          borderWidth={1}
+          borderRadius={20}
+          style={{ overflow: "hidden" }}
+          size={20}
+          label="I agree with the general terms and conditions"
+        />
+        <Checkbox
+          checked={false}
+          icon={
+            <div
+              style={{
+                display: "flex",
+                flex: 1,
+                backgroundColor: "#0094FF",
+                alignSelf: "stretch",
+              }}
+            >
+              <Icon.FiCheck color="white" size={20} />
+            </div>
+          }
+          borderColor="#174A41"
+          borderWidth={1}
+          borderRadius={20}
+          style={{ overflow: "hidden" }}
+          size={20}
+          label="I agree to the sending of marketing news"
+        />
+      </div>
+
+      <button className={" py-2 rounded-lg bg-blue-500"}>
+        <div className="flex items-center justify-center gap-2 text-white">
+          <img src={user} alt="" className="w-3 h-3 object-contain" />
+          <p>Register</p>
+        </div>
+      </button>
+      <div className="flex md:hidden justify-center">
+        <button className="font-medium px-1 py-[0.5px] border-[1px] rounded-md border-blue-500 text-blue-500 bg-[#F0F2F5] text-center">
+          Close
+        </button>
+      </div>
+    </div>
+  )
 }
 
 function SignInModal() {
@@ -277,7 +416,7 @@ function FormInput({ label, type, placeholder, color = 1 }) {
   if (color == 2) border = " border-blue-500";
 
   return (
-    <div className="flex flex-col gap-1 w-[300px] md:w-[360px]">
+    <div className="flex flex-col gap-1 md:w-[360px]">
       <label htmlFor="" className="font-medium">
         {label}
       </label>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { slide as Menu } from "react-burger-menu";
-import {Container} from "./Container";
+import { Container } from "./Container";
 import logo from "./../assets/logo.png";
 import user_icon from "./../assets/user_icon.png";
+import arrow_down_icon from "./../assets/arrow_down_icon.png";
 import flag_1 from "./../assets/flag_1.png";
+import flag_2 from "./../assets/flag_2.png";
+import flag_3 from "./../assets/flag_3.png";
+import flag_4 from "./../assets/flag_4.png";
+import tick_icon from "./../assets/tick_icon.png";
+
 import burger_icon from "./../assets/burger_icon.png";
 import "./Navbar.css";
 
@@ -36,7 +42,7 @@ export function Navbar() {
             </a>
           </div>
         </div>
-        <div className="w-[48%] lg:flex-1 flex justify-end items-center gap-4 lg:gap-8">
+        <div className="w-[48%] lg:w-[60%] flex justify-end items-center gap-4 lg:gap-8">
           <button className="hidden lg:block px-2 py-2 border-2 rounded-[8px] bg-[#F0F2F5] border-blue-500 text-blue-500">
             <p className="text-base font-normal">Login</p>
           </button>
@@ -48,14 +54,74 @@ export function Navbar() {
               </p>
             </div>
           </button>
-          <button className=" lg:px-2 lg:py-2 lg:border-2 rounded-[8px] bg-white border-blue-500 text-black">
-            <div className="flex items-center gap-2">
-              <img src={flag_1} alt="" className="w-9 lg:w-7 h-fit" />
-              <p className="hidden lg:block text-base font-normal">SVK</p>
-            </div>
-          </button>
+          <LanguageDropdown />
         </div>
       </Container>
     </>
+  );
+}
+
+function LanguageDropdown() {
+  const [isDropdownHidden, setIsDropdownHidden] = useState(true);
+  const [flag, setFlag] = useState(flag_1);
+  const [language, setLanguage] = useState("SVK");
+
+  function dropdownHandler() {
+    setIsDropdownHidden(!isDropdownHidden);
+  }
+
+  function languageHandler(langName, langFlag){
+    setLanguage(langName)
+    setFlag(langFlag)
+    setIsDropdownHidden(true);
+  }
+
+  return (
+    <div className="w-[110px] flex flex-col relative">
+      <button
+        onClick={dropdownHandler}
+        className="flex justify-around items-center gap-2 lg:px-2 lg:py-2 lg:border-2 rounded-[8px] bg-white border-blue-500 text-black"
+      >
+        <div className="flex items-center gap-2">
+          <img src={flag} alt="" className="w-9 lg:w-7 h-fit rounded" />
+          <p className="hidden lg:block text-base font-normal">{language}</p>
+        </div>
+        <img src={arrow_down_icon} alt="" className="h-5 w-5" />
+      </button>
+      {isDropdownHidden ? (
+        ""
+      ) : (
+        <div className="w-[130px] max-h-[200px] overflow-auto flex flex-col gap-2 py-1 absolute top-[50px] right-0 border-[1px] rounded-[8px] bg-white border-blue-500 z-10">
+          <LanguageOption flag={flag_1} name={"SVK"} isSelected={language==="SVK"} onLanguageChange={languageHandler} />
+          <LanguageOption flag={flag_2} name={"SLO"} isSelected={language==="SLO"} onLanguageChange={languageHandler}/>
+          <LanguageOption flag={flag_3} name={"AUS"} isSelected={language==="AUS"} onLanguageChange={languageHandler}/>
+          <LanguageOption flag={flag_4} name={"ITA"} isSelected={language==="ITA"} onLanguageChange={languageHandler}/>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function LanguageOption({ flag, name, isSelected = false, onLanguageChange }) {
+
+  function clickHandker(){
+    onLanguageChange(name, flag)
+
+  }
+
+  return (
+    <button onClick={clickHandker} className="flex  items-center gap-2 lg:px-2 lg:py-2 text-black hover:bg-[#F4F9FF]">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-2">
+          <img src={flag} alt="" className="w-9 lg:w-7 h-fit rounded" />
+          <p className="hidden lg:block text-base font-normal">{name}</p>
+        </div>
+        {isSelected ? (
+          <img src={tick_icon} alt="" className="w-5 h-fit mr-4" />
+        ) : (
+          ""
+        )}
+      </div>
+    </button>
   );
 }
